25b34ed2f57feec295c5ae2c7d9e55150e85eeb1
github username <github email address>
2019-09-21 00:19:37 -0400

    Done.

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,29 @@
 class Painting
 
   attr_reader :title, :price
+  attr_accessor :artist, :gallery
 
   @@all = []
 
-  def initialize(title, price)
+  def initialize(artist, gallery, title, price)
+    @artist = artist
+    @gallery = gallery
     @title = title
     @price = price
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.total_price
+    sum = 0
+    total = self.all.map do |painting|
+      painting.price
+    end
+    sum = total.inject { |sum, n| sum + n }  
   end
 
 end
+ 
--- tools/console.rb
+++ tools/console.rb
@@ -1,5 +1,17 @@
 require_relative '../config/environment.rb'
 
+art1 = Artist.new("Joe", 10)
+art2 = Artist.new("Steve", 154342)
+art3 = Artist.new("Alex", 400)
+gal1 = Gallery.new("G1", "London")
+gal2 = Gallery.new("G2", "Paris")
+gal3 = Gallery.new("G3", "New York")
+ptng1 = Painting.new(art1, gal1, "Mona Lisa", 300)
+ptng2 = Painting.new(art1, gal3, "Bears", 100000)
+ptng3 = Painting.new(art2, gal2, "Cool Painting", 30)
+ptng4 = Painting.new(art3, gal2, "Nice Drawing", 690)
+ptng5 = Painting.new(art3, gal1, "PP", 8500)
+ptng6 = Painting.new(art3, gal3, "Yo", 4100)
 
 
 
