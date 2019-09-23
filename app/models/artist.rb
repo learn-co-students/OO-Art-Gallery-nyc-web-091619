575c3f4fedfa54fa339f0face69a6bd9b228fb5c
Jonathan Tan <you@example.com>
2019-09-23 10:15:10 -0400

    done

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,62 @@
 class Artist
 
-  attr_reader :name, :years_experience
+    @@all = []
 
-  def initialize(name, years_experience)
-    @name = name
-    @years_experience = years_experience
-  end
+    attr_reader :name, :years_experience
+
+    def initialize(name, years_experience)
+      @name = name
+      @years_experience = years_experience
+      @@all << self 
+    end
+
+      def self.all 
+        @@all 
+    end 
+
+    def my_paintings
+      Painting.all.select do |painting|
+      painting.artist == self 
+      end 
+    end 
+
+    def my_galleries 
+      my_paintings.map do |painting|
+        painting.gallery 
+      end 
+    end 
+
+    def cities 
+      my_galleries.map do |gallery|
+        gallery.city 
+      end 
+    end 
+
+  
+    def self.total_experience 
+      all_years = self.all.map do |artist|
+                  artist.years_experience
+                  end 
+      all_years.reduce(0) do |sum, n|
+        sum + n 
+      end 
+    end  
+
+    def self.most_prolific 
+      self.all.max_by do |artist|
+        artist.my_paintings.length / artist.years_experience 
+      end
+    end 
+
+
+
+    def create_painting(title, price, gallery)
+      Painting.new(title, price, self, gallery)
+    end
+
+
+
+
+
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,5 +1,17 @@
 require_relative '../config/environment.rb'
 
+a1 = Artist.new("jon",10)
+a2 = Artist.new("kim",15)
+a3 = Artist.new("sofie",20)
+
+g1 = Gallery.new("MOMA", "NYC")
+g2 = Gallery.new("MFA", "BOS")
+g3 = Gallery.new("Louvre", "Paris")
+
+p1 = Painting.new("Mona Lisa", 10000000, a1, g3)
+p2 = Painting.new("Apple painting", 20000, a2, g1)
+p3 = Painting.new("bowl", 40, a3, g2)
+p4 = Painting.new("sky", 500000, a1, g1)
 
 
 
